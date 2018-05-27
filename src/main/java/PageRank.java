04f603bc8d5b8df90f81906aa96113bd34493ddd
Eric Wen <eericwen@icloud.com>
2018-05-27 22:53:54 +0800

    [+] add entity ID validation

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PageRank {
 
@@ -8,14 +10,21 @@ public class PageRank {
 		final int id;
 		final List<Integer> linkIDs = new ArrayList<>();
 
+		double rankValue = 1.0;
+
 		Entity(int id) {
 			this.id = id;
+		}
+
+		int getID() {
+			return id;
 		}
 
 	}
 
 	private final double alpha;
 	private final List<Entity> entities = new ArrayList<>();
+	private final Map<Integer, Integer> indexMap = new HashMap<>();
 
 	public PageRank() {
 		this.alpha = 0.85;
@@ -30,13 +39,36 @@ public class PageRank {
 		this.alpha = alpha;
 	}
 
+	private Entity getEntityWithID(int id) {
+		Integer entityIndex = indexMap.get(id);
+		return entityIndex == null? null: entities.get(entityIndex);
+	}
+
+	private boolean isValidID(int id) {
+		Entity entityWithID = getEntityWithID(id);
+		return entityWithID != null;
+	}
+
+	private void validateId(int id, String argName) {
+		if (!isValidID(id)) {
+			throw new IllegalArgumentException(String.format(
+					"Illegal argument '%s': invalid ID %d.", argName, id));
+		}
+	}
+
 	public void addRef(int fromID, int toID) {
+		validateId(fromID, "fromID");
+		validateId(toID, "toID");
 	}
 
 	public void removeRef(int fromID, int toID) {
+		validateId(fromID, "fromID");
+		validateId(toID, "toID");
 	}
 
 	public boolean hasRef(int fromID, int toID) {
+		validateId(fromID, "fromID");
+		validateId(toID, "toID");
 		return false;
 	}
 
@@ -45,20 +77,22 @@ public class PageRank {
 	}
 
 	public void destroyEntity(int id) {
+		validateId(id, "id");
 	}
 
 	public void updateRankValue() {
 	}
 
 	public double getRankValue(int id) {
+		validateId(id, "id");
 		return 0.0;
 	}
 
-	public boolean isValid(int id) {
+	public boolean isRankValueValid(int id) {
 		return true;
 	}
 
-	public boolean isUpToDate(int id) {
+	public boolean isRankValueUpToDate(int id) {
 		return true;
 	}
 

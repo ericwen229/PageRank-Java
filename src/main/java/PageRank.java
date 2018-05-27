0d6ef599d5475070bb2a4a5350a0a0bb888b2c0d
Eric Wen <eericwen@icloud.com>
2018-05-27 22:30:31 +0800

    [+] add basic structures for storing link data

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,11 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PageRank {
 
-	public PageRank() {
+	private class Entity {
+
+		final int id;
+		final List<Integer> linkIDs = new ArrayList<>();
+
+		Entity(int id) {
+			this.id = id;
+		}
+
 	}
 
-	public void addRef(int fromID, int toID) {}
+	private final double alpha;
+	private final List<Entity> entities = new ArrayList<>();
 
-	public void removeRef(int fromID, int toID) {}
+	public PageRank() {
+		this.alpha = 0.85;
+	}
+
+	public PageRank(double alpha) {
+		if (alpha < 0 || alpha > 1) {
+			throw new IllegalArgumentException(String.format(
+					"Illegal argument 'alpha': number inside the intervel [0.0, 1.0] expected, " +
+							"%.2f provided.", alpha));
+		}
+		this.alpha = alpha;
+	}
+
+	public void addRef(int fromID, int toID) {
+	}
+
+	public void removeRef(int fromID, int toID) {
+	}
 
 	public boolean hasRef(int fromID, int toID) {
 		return false;
@@ -15,9 +44,11 @@ public class PageRank {
 		return 0;
 	}
 
-	public void destroyEntity(int id) {}
+	public void destroyEntity(int id) {
+	}
 
-	public void updateRankValue() {}
+	public void updateRankValue() {
+	}
 
 	public double getRankValue(int id) {
 		return 0.0;

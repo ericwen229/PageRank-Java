8469557d5af41714938cd9be5db6419b2a4c6900
Eric Wen <eericwen@icloud.com>
2018-06-02 22:24:51 +0800

    [*] optimize imports

--- src/main/java/com/github/ericwen229/PageRank.java
+++ src/main/java/com/github/ericwen229/PageRank.java
@@ -1,6 +1,10 @@
 package com.github.ericwen229;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PageRank {
 
